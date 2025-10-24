ab1ec0eded7509b522a3e489307cd6aa9c10732b
MarquezLuis96 <englamc@gmail.com>
2025-10-24 11:47:10 -0400

    pre pagination

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Configure the ConfigModule to load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true, // Optional: Makes ConfigService available globally
+    }),
+
+    // 2. Configure TypeOrmModule using the environment variables
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT! || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      // ... other TypeORM options
+    }),
+
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/products/products.service.ts
@@ -0,0 +1,64 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  BadRequestException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductsService {
+  private readonly logger = new Logger('ProductsServices');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      const newProduct = await this.productRepository.save(product);
+      return {
+        message: 'Producto creado correctamente.',
+        product: newProduct,
+      };
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async findAll() {
+    return await this.productRepository.find();
+  }
+
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException('Producto no encontrado');
+    }
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    throw new InternalServerErrorException('Ayuda!');
+  }
+}
